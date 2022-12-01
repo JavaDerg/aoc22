6948221a62e0193ff4345cd9a61a2bd409c0446d
post-rex <post-rex@pm.me>
2022-12-01 14:11:57 +0100

    over-engineering it

--- days/day1/src/main.rs
+++ days/day1/src/main.rs
@@ -1,25 +1,24 @@
+use itertools::Itertools;
+
 fn main() {
-    let mut iter = include_str!("../input.txt").lines().map(|x| x.trim());
+    let mut items = include_str!("../input.txt")
+        .lines()
+        .map(|x| x.trim())
+        .scan(0, |mut g, n| {
+            if n.is_empty() {
+                *g += 1;
+                Some(None)
+            } else {
+                Some(Some((*g, n.parse::<u64>().unwrap())))
+            }
+        })
+        .filter_map(|x| x)
+        .group_by(|(g, _)| *g)
+        .into_iter()
+        .map(|(_, g)| g.into_iter().map(|(_, n)| n).sum::<u64>())
+        .sorted()
+        .collect::<Vec<u64>>();
 
-    let mut max = 0;
-    let mut acc = vec![];
-    while let x @ 1.. = total_cal_rec(&mut iter) {
-        max = max.max(x);
-        acc.push(x);
-    }
-    acc.sort();
-
-    println!("{}", acc.iter().rev().take(3).sum::<u64>());
-}
-
-fn total_cal_rec<'a>(mut i: impl Iterator<Item = &'a str>) -> u64 {
-    if let Some(n) = i.next() {
-        if n.is_empty() {
-            0
-        } else {
-            n.parse::<u64>().unwrap() + total_cal_rec(i)
-        }
-    } else {
-        0
-    }
+    println!("p1: {}", items.last().unwrap());
+    println!("p2: {}", items.iter().rev().take(3).sum::<u64>());
 }
