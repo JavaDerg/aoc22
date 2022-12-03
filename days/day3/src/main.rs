ace93e08080489c2a3e6c447a45f4d56d2720622
post-rex <post-rex@pm.me>
2022-12-03 12:07:09 +0100

    d3 p2

--- days/day3/src/main.rs
+++ days/day3/src/main.rs
@@ -8,30 +8,25 @@ fn main() {
 
     let mut acc = 0;
 
-    for l in input.lines() {
-        let s = l.len() / 2;
-
-        let (d1, d2) = (
-                &l[..s],
-                &l[s..],
-            );
-
-        let mut hm = HashMap::new();
-        for (k, _) in d1.chars().counts() {
-            hm.insert(k, 1);
-        }
-        for (k, _) in d2.chars().counts() {
-            *hm.entry(k).or_insert(0) += 1;
-        }
-
-        hm.drain_filter(|k, v| *v == 1);
-
-        let shared = hm.iter().next().unwrap().0;
-
-        acc += prio(*shared);
+    for (l1, l2, l3) in input.lines().tuples() {
+        acc += prio(find_shared(&[l1, l2, l3]));
     }
 
     println!("{}", acc);
+}
+
+
+fn find_shared(input: &[&str]) -> char {
+    let mut hm = HashMap::new();
+    for str in input {
+        for (k, _) in str.chars().counts() {
+            *hm.entry(k).or_insert(0) += 1;
+        }
+    }
+
+    hm.drain_filter(|k, v| *v < input.len());
+
+    hm.into_iter().next().unwrap().0
 }
 
 fn prio(c: char) -> u32 {
